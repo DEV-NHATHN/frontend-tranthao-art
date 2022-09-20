cee5459f71e5ad2a92a7e2451e152fac8617e38c
DEV-NHATHN <ongthosongtu@gmail.com>
2022-09-20 23:13:36 +0700

    feat: favicon

--- src/components/Metatags.tsx
+++ src/components/Metatags.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 
 export default function Metatags({
-  title = `Leo`,
+  title = 'Leo',
   description = `3D Visual Designs and Art Portfolio`,
   image = `https://res.cloudinary.com/dcjxcptdt/image/upload/v1663565697/tranthao/udbfxehjbqacrtauxblz.jpg`,
 }) {
+  const content = `Corgiphy | ${title}`;
   return (
     <Head>
-      <title>Corgiphy | {title}</title>
+      <title>{content}</title>
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@tranthaoart" />
       <meta name="twitter:title" content={title} />
@@ -18,13 +19,15 @@ export default function Metatags({
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
 
-      <meta http-equiv="Content-Type" content="text / html; charset = utf-8" />
+      <meta httpEquiv="Content-Type" content="text / html; charset = utf-8" />
       {/* <meta
-        http-equiv="refresh"
+        httpEquiv="refresh"
         content="5;url=https://www.instagram.com/tranthao.art/"
       /> */}
       <meta name="robots" content="noindex" />
       <meta name="viewport" content="width = device-width, initial-scale = 1" />
+
+      <link rel="shortcut icon" href="/corgi.png" />
     </Head>
   );
 }
